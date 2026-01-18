cad36667698c9feee668574e343a2fe3476d63a8
Sheral18 <sheralwaskar18@gmail.com>
2026-01-18 11:58:15 +0530

    Add devise and create users

---
+++ test/controllers/welcome_controller_test.rb
@@ -0,0 +1,12 @@
+require 'testhelper'
+
+class WelcomeControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get index" do
+    get welcome_index_url
+
+    assert_response :success
+
+  end
+
+end
